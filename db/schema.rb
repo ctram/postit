cdf80ef45f70d5da642eee6091978907ca65effd
Chris Tram <chris.tram@gmail.com>
2015-02-18 06:24:12 -0800

    Add vote table. Associated votes with users(the owner of votes), posts, and comments through polymorphic relation.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150216073150) do
+ActiveRecord::Schema.define(version: 20150218140729) do
 
   create_table "categories", force: true do |t|
     t.string "name"
@@ -46,6 +46,15 @@ ActiveRecord::Schema.define(version: 20150216073150) do
     t.datetime "updated_at"
   end
 
+  create_table "tables", force: true do |t|
+    t.boolean  "vote"
+    t.integer  "user_id"
+    t.string   "voteable_type"
+    t.integer  "voteable_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "teams", force: true do |t|
     t.string "name"
   end
@@ -66,6 +75,15 @@ ActiveRecord::Schema.define(version: 20150216073150) do
     t.string "username"
     t.string "password_digest"
     t.string "time_zone"
+  end
+
+  create_table "votes", force: true do |t|
+    t.boolean  "vote"
+    t.integer  "user_id"
+    t.string   "voteable_type"
+    t.integer  "voteable_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
