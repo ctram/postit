67409c313622ea42c6850c1add4662b1c2133248
Chris Tram <chris.tram@gmail.com>
2015-02-23 19:34:39 -0800

    Add todo items.

--- todos.rb
+++ todos.rb
@@ -2,4 +2,20 @@
 # TODO: Add mailbox for each user
 # TODO: User can send messages to each other
 # TODO: Make comments voteable
-# TODO: Comments and posts move up and down the ladder based on vote totals.
+# TODO: Comments and posts move up and down the ladder based on vote totals
+
+# TODO: User must be logged in in order to vote -- lock on votes_controller actions.
+
+
+# XXX: Chris coded voting by doing:
+      # resources :posts, except: [:destroy] do
+      #   member do
+      #     post :vote
+      #   end
+      #
+      #   resources :comments, only: [:create]
+      # end
+      #
+      # resources :categories, only: [:new, :create, :show]
+
+  # you added routes manually.
