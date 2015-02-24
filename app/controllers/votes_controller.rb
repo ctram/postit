c845ec9ed6b4debce1010f7bae562a0fc40f5e8a
Chris Tram <chris.tram@gmail.com>
2015-02-23 17:30:03 -0800

    User can vote at most +1 or -1 for each post object.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,16 +1,34 @@
 class VotesController < ApplicationController
   def up_vote_post
     @post = Post.find(params[:id])
+    @posts = Post.all
     @vote = Vote.new(vote:true, user_id: session[:user_id], voteable_type:'Post', voteable_id: @post.id)
-    @post.votes << @vote
-    redirect_to posts_path
+
+    user_net_votes = Vote.sum_votes_by_user_by_voteable_id @vote.voteable_id, User.find(session[:user_id])
+
+    if user_net_votes + 1 <= 1
+      @post.votes << @vote
+      redirect_to posts_path
+    else
+      flash[:error] = "You cannot up vote this item anymore. You can still down vote, though."
+      render '/posts/index'
+    end
   end
 
   def down_vote_post
     @post = Post.find(params[:id])
+    @posts = Post.all
     @vote = Vote.new(vote:false, user_id: session[:user_id], voteable_type:'Post', voteable_id: @post.id)
-    @post.votes << @vote
-    redirect_to posts_path
+
+    user_net_votes = Vote.sum_votes_by_user_by_voteable_id @vote.voteable_id, User.find(session[:user_id])
+
+    if user_net_votes - 1 >=  -1
+      @post.votes << @vote
+      redirect_to posts_path
+    else
+      flash[:error] = "You cannot down vote this item anymore. You can still up vote, though."
+      render '/posts/index'
+    end
   end
 
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,22 @@
 class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :voteable, polymorphic:true
+  # TODO: User can only change the total vote by +1 or -1 max.
+
+  def Vote.sum_votes_by_user_by_voteable_id voteable_id, user
+    user_votes = Vote.all.select do |vote|
+      (vote.voteable_id == voteable_id) and (vote.user_id == user.id)
+    end
+    # return net votes by this user.
+    net_votes = user_votes.reduce(0) do |memo,vote|
+
+      if vote.vote
+        memo += 1
+      else
+        memo -= 1
+      end
+    end
+    net_votes
+  end
+
 end
